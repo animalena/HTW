fcf936a239ca8257909419411eef9cee143a20e5
animalena <anna.hunold@googlemail.com>
2014-07-07 16:05:23 +0200

    solved some issues with the customer show page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140616081634) do
+ActiveRecord::Schema.define(version: 20140630151528) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140616081634) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.decimal  "price"
   end
 
   create_table "pumpkins", force: true do |t|
