1d830c18a4279c05c62fbbac064fde7c7e2b869b
animalena <anna.hunold@googlemail.com>
2014-07-07 19:59:40 +0200

    final changes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -10,8 +10,6 @@ class CustomersController < ApplicationController
   # GET /customers/1
   # GET /customers/1.json
   def show
-    @orders = Customer.where(name: @customer.name).first.orders
-
     @orders = @customer.orders
     @total_price = @customer.orders.sum(:price)
   end
